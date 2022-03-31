ebd4f6a9607197937f3e9578384a0e45917e5408
Andres Roca P <afrokap@gmail.com>
2022-03-30 22:15:56 -0500

    iterating through cells | writing file

--- xlsx-files/true-action/mntOtFiller.py
+++ xlsx-files/true-action/mntOtFiller.py
@@ -6,9 +6,14 @@ print('-\n---\n-----\n-------\n-----\n---\n-')
 originFileName = 'del drive - MNN-REG-046 Solicitudes de Mantenimiento.xlsx'
 originFile = openpyxl.load_workbook('del drive - MNN-REG-046 Solicitudes de Mantenimiento.xlsx')
 originSheet = originFile['Solicitudes']
-otNumber = str(int(originSheet['J2025'].value))
-otDate = str(originSheet['B2025'].value).split(' ')[0]
-otDetail = str(originSheet['E2025'].value)
-otType = str(originSheet['D2025'].value)
-print(f'{otNumber}\n{otDate}\n{otDetail}\n{otType}')
 
+for row in range(2025,2033 + 1):
+    otNumber = str(int(originSheet[f'J{row}'].value))
+    otDate = str(originSheet[f'B{row}'].value).split(' ')[0]
+    otDetail = str(originSheet[f'E{row}'].value)
+    otType = str(originSheet[f'D{row}'].value)
+    originSheet[f'C{row}'] = 'hello .xlsx'
+    print(f'{otNumber}\n{otDate}\n{otDetail}\n{otType}')
+    print('-')
+
+originFile.save(filename=originFileName)
