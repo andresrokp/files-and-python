e8a69dcbde162c72cf0e1392f2cab05e2c06eb08
Andres Roca P <afrokap@gmail.com>
2022-04-30 05:39:13 -0500

    delete/create file | new col | organize script

--- xlsx-files/lubrication-concat/lubeConcatener.py
+++ xlsx-files/lubrication-concat/lubeConcatener.py
@@ -2,77 +2,65 @@
 import openpyxl
 import os
 
-print('Hello concatener')
+print('\nHello concatener')
 print('-\n---\n-----\n-------\n-----\n---\n-')
 print('Hello concatener\n')
 
-
+# nombramientos
 dataFileName = 'lubeDataFile.xlsx'
-if os.path.exists('out-'+dataFileName) : os.remove('out-'+dataFileName)
 inputSheetName = 'PREVENTIVO_LUBRICACIÓN'
 outSheetName = 'AMxEQ'
+outFileName = 'out-lubeDataFile.xlsx'
 
+# declaración de filas
 inputSheetBeginRow = 14
 inputSheetEndRow = 24
 outSheetBeginRow = 4
 
+# traer data al workspace
 dataFile = openpyxl.load_workbook(dataFileName)
 originSheet = dataFile[inputSheetName]
 outSheet = dataFile[outSheetName]
 
+# iteración sobre filas / row iterator
 for row in range(inputSheetBeginRow,inputSheetEndRow + 1):
-    
     # getting values from origin file
     sistema = str(originSheet[f'C{row}'].value)
     equipo = str(originSheet[f'E{row}'].value)
     posicion = str(originSheet[f'F{row}'].value)
     lubricante = str(originSheet[f'G{row}'].value)
-    cantPorUnid = str(float(originSheet[f'H{row}'].value))
-    cantTotal = str(float(originSheet[f'I{row}'].value))
-    cantTotalFactor = type(cantTotal) # cantTotal.split('*')[1]
+    cantPorUnid = float(originSheet[f'H{row}'].value)
+    cantTotal = float(originSheet[f'I{row}'].value)
     tarea = str(originSheet[f'J{row}'].value)
-    freqSemanas = str(int(originSheet[f'K{row}'].value))
-    tiempoMinu = str(int(originSheet[f'N{row}'].value))
+    freqSemanas = int(originSheet[f'K{row}'].value)
+    tiempoMinu = int(originSheet[f'N{row}'].value)
     estadoMaq = str(originSheet[f'R{row}'].value)
     tipoLube = str(originSheet[f'S{row}'].value)
-    
     print(f'{row} :: {sistema}\t{equipo}\t{lubricante}\t{cantPorUnid}\t{cantTotal}\t{tarea}\t{freqSemanas}\t{tiempoMinu}\t{estadoMaq}\t{tipoLube}')
     
-    
+    # contruyendo valores de salida / assemblying output values
     unidadLube = 'gr' if tipoLube == 'GRASA' else 'lt'
-    numPuntos = float(cantTotal)/float(cantPorUnid)
+    numPuntos = int(cantTotal/cantPorUnid)
     tipoHta = 'Engrasadora' if tipoLube == 'GRASA' else 'Oil safe (Recipiente)'
-    
     descripcion = f"""{tarea}
     Lubricante: {lubricante}
     Cantidad: {cantPorUnid} {unidadLube} (Por punto)
     # Puntos: {numPuntos}
     Herramienta: {tipoHta}
     """
-
-    nombreEquipo = f'{equipo} : {posicion} ({sistema})(BOPP)'
+    nombreEquipo = f'{equipo} : {posicion} ({sistema}) (BOPP)'
     reqOper = 'En Operación' if estadoMaq == 'FUNCIONANDO' else 'Parado por Mantenimiento'
+    freqDias = freqSemanas * 7
 
     # writing at outSheet
     outRow = row - 10;
     outSheet[f'B{outRow}'] = tarea
     outSheet[f'C{outRow}'] = descripcion
     outSheet[f'E{outRow}'] = nombreEquipo
+    outSheet[f'M{outRow}'] = reqOper
 
     print('-')
 
-dataFile.save(filename='out-'+dataFileName)
-
-    # out file creatinon and loading
-    # outFileName = f'OT {otNumber}.xlsx'
-    # outFile = openpyxl.load_workbook('OT 0000.xlsx')
-    # outSheet = outFile[outSheetName]
-
-    # output workbook writing
-    # outSheet['R6'] = otSolNum
-    # outSheet['G7'] = "IVÁN DE ALBA" if otEng == 'MECANICA' else 'JORGE MOLINA'
-    # if otType == 'PREVENTIVO': outSheet['N14'] = 'x'
-    
-    # outFile.save(filename=outFileName)
-    
-
+# deleting and creating out file
+if os.path.exists(outFileName) : os.remove(outFileName)
+dataFile.save(filename=outFileName)
