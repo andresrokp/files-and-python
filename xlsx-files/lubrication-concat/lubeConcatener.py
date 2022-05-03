748c5f2b414286d1d675f73a4acd29e01a11ea60
Andres Roca P <afrokap@gmail.com>
2022-05-03 06:15:38 -0500

    all colmuns ready :)

--- xlsx-files/lubrication-concat/lubeConcatener.py
+++ xlsx-files/lubrication-concat/lubeConcatener.py
@@ -1,6 +1,7 @@
 
 import openpyxl
 import os
+import math
 
 print('\nHello concatener')
 print('-\n---\n-----\n-------\n-----\n---\n-')
@@ -51,14 +52,28 @@ for row in range(inputSheetBeginRow,inputSheetEndRow + 1):
     nombreEquipo = f'{equipo} : {posicion} ({sistema}) (BOPP)'
     reqOper = 'En Operación' if estadoMaq == 'FUNCIONANDO' else 'Parado por Mantenimiento'
     freqDias = freqSemanas * 7
-
+    hardcodedList = ['Media','Lubricacion','Sistemática','Flotante','Tiempo']
+    holgura = 0;
+    if freqDias <= 20:
+        holgura = math.ceil(0.2*freqDias)
+    elif freqDias <= 60:
+        holgura = math.ceil(0.15*freqDias)
+    elif freqDias <= 120:
+        holgura = math.ceil(0.10*freqDias)
     # writing at outSheet
     outRow = row - 10;
     outSheet[f'B{outRow}'] = tarea
     outSheet[f'C{outRow}'] = descripcion
     outSheet[f'E{outRow}'] = nombreEquipo
+    outSheet[f'H{outRow}'] = hardcodedList[0]
+    outSheet[f'I{outRow}'] = hardcodedList[1]
+    outSheet[f'J{outRow}'] = hardcodedList[2]
+    outSheet[f'K{outRow}'] = hardcodedList[3]
+    outSheet[f'L{outRow}'] = hardcodedList[4]
     outSheet[f'M{outRow}'] = reqOper
-
+    outSheet[f'N{outRow}'] = freqDias
+    outSheet[f'O{outRow}'] = holgura
+    outSheet[f'R{outRow}'] = tiempoMinu
     print('-')
 
 # deleting and creating out file
